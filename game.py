b58e1c5b73bf8460184c72686b11c056f51bbdd2
alvin1785 <alvin1785@gmail.com>
2025-07-31 14:22:57 +0900

    [refactor] ~p50

--- game.py
+++ game.py
@@ -2,21 +2,18 @@ from mimetypes import guess_type
 
 
 class Game:
-    def guess(self, guessNumber):
-        if guessNumber is None:
-            raise TypeError
+    def guess(self, guess_number):
+        if guess_number is None:
+            raise TypeError("입력이 None입니다.")
 
-        if len(guessNumber) != 3:
-            raise TypeError
+        if len(guess_number) != 3:
+            raise TypeError("입력은 3자리 문자열이어야 합니다.")
 
-        for number in guessNumber:
-            if not ord('0') <= ord(number) <= ord('9'):
-                raise TypeError
+        if not guess_number.isdigit():
+            raise TypeError("입력 문자열은 모두 숫자여야합니다.")
 
-        if self.isDuplicatedNumber(guessNumber):
-            raise TypeError
+        if self._is_duplicated_number(guess_number):
+            raise TypeError("입력 숫자 중 중복이 존재합니다.")
 
-    def isDuplicatedNumber(self, guessNumber):
-        return guessNumber[0] == guessNumber[1] or \
-            guessNumber[0] == guessNumber[2] or \
-            guessNumber[1] == guessNumber[2]
+    def _is_duplicated_number(self, guessNumber):
+        return len(set(guessNumber)) != 3
