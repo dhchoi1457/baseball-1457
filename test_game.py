28ecf9dbaa6647e42ae4ecf31c494d50fc772355
alvin1785 <alvin1785@gmail.com>
2025-07-31 14:07:24 +0900

    [refactor] p35

--- test_game.py
+++ test_game.py
@@ -2,16 +2,23 @@ import pytest
 
 from game import Game
 
+
 @pytest.fixture()
 def game():
     return Game()
+
 
 def test_exception_when_input_is_none(game):
     with pytest.raises(TypeError):
         game.guess(None)
 
+
 def test_exception_when_input_length_unmatched(game):
     guessNumber = "12"
+    assert_illegal_argument(game, guessNumber)
+
+
+def assert_illegal_argument(game, guessNumber):
     try:
         game.guess(guessNumber)
         pytest.fail()
