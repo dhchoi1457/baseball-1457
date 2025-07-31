2fafa3409b508edcdb3b2a4618698d6b14aefd3b
alvin1785 <alvin1785@gmail.com>
2025-07-31 14:12:38 +0900

    [feature] ~p40

--- game.py
+++ game.py
@@ -7,3 +7,7 @@ class Game:
             raise TypeError
         if len(guessNumber) != 3:
             raise TypeError
+        for number in guessNumber:
+            if not ord('0')  <= ord(number) <= ord('9'):
+                raise TypeError
+
