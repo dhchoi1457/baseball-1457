472b4fa363b5cd093dff57a154a4039f2b326655
alvin1785 <alvin1785@gmail.com>
2025-07-31 14:36:16 +0900

    [feature] ~p52

--- game.py
+++ game.py
@@ -1,8 +1,10 @@
 from mimetypes import guess_type
 
+from game_result import GameResult
+
 
 class Game:
-    def guess(self, guess_number):
+    def guess(self, guess_number) -> GameResult:
         if guess_number is None:
             raise TypeError("입력이 None입니다.")
 
@@ -14,6 +16,8 @@ class Game:
 
         if self._is_duplicated_number(guess_number):
             raise TypeError("입력 숫자 중 중복이 존재합니다.")
+
+        return GameResult(True, 3, 0)
 
     def _is_duplicated_number(self, guessNumber):
         return len(set(guessNumber)) != 3
---
+++ game_result.py
@@ -0,0 +1,7 @@
+class GameResult:
+    def __init__(self, solved, strikes, balls):
+        self.solved = solved
+        self.strikes = strikes
+        self.balls = balls
+
+    pass
