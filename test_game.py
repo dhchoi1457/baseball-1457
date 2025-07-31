bb5e59d8b8a70ccba2e90ef35dc35dcb37548ad0
alvin1785 <alvin1785@gmail.com>
2025-07-31 14:09:50 +0900

    [refactor] ~p39

--- test_game.py
+++ test_game.py
@@ -8,19 +8,15 @@ def game():
     return Game()
 
 
-def test_exception_when_input_is_none(game):
-    with pytest.raises(TypeError):
-        game.guess(None)
-
-
-def test_exception_when_input_length_unmatched(game):
-    guessNumber = "12"
-    assert_illegal_argument(game, guessNumber)
-
-
 def assert_illegal_argument(game, guessNumber):
     try:
         game.guess(guessNumber)
         pytest.fail()
     except TypeError:
         pass
+
+
+def test_exception_when_invalid_input(game):
+    assert_illegal_argument(game, None)
+    assert_illegal_argument(game, "12")
+    assert_illegal_argument(game, "1234")
